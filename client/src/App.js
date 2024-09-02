dac529f4d21885d889d66e3f88a91c30717bce42
ThanziaPatelRaheem <thanzia.pr@gmail.com>
2024-09-02 17:20:12 +0800

    added url paramters

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 //Pages
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import ArticleList from "./pages/ArticleList";
+import Article from "./pages/Article";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <div className="max-w-screen-md mx-auto pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles-list" element={<ArticleList />} />
+          <Route path="/article/:name" element={<Article />} />
         </Routes>
       </div>
     </Router>
