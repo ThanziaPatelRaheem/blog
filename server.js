60aff85a48a477a6cef3ccde2c66b58a1186b3e0
ThanziaPatelRaheem <thanzia.pr@gmail.com>
2024-09-02 21:52:40 +0800

    adding comment functionality

--- server.js
+++ server.js
@@ -2,8 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = process.env.port || 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+const articlesInfo = {
+  "learn-react": {
+    comments: [],
+  },
+  "learn-node": {
+    comments: [],
+  },
+  "my-thoughts-on-learning-react": {
+    comments: [],
+  },
+};
+
+app.use(express.json({ extended: false }));
+
+app.post("/api/articles/:name/add-comments", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
 });
 app.listen(PORT, () => {
   console.log(`Server is connected to ${PORT}`);
