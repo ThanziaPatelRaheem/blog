990810c3edd34473e965e43d09e8f2b4dc2dcf12
ThanziaPatelRaheem <thanzia.pr@gmail.com>
2024-09-03 13:54:38 +0800

    connecting mongodb to express

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const app = express();
+const { MongoClient } = require("mongodb");
 const PORT = process.env.port || 8000;
 
 app.use(express.json({ extended: false }));
+
+app.get("/api/articles/:name", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+    const client = await MongoClient.connect("mongodb://localhost:27017");
+    const db = client.db("mernblog");
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to database", error });
+  }
+});
 
 app.post("/api/articles/:name/add-comments", (req, res) => {
   const { username, text } = req.body;
